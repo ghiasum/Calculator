425a49ebfdec882f953af745a6d21fd532b59d25
MGhias <ghiasum@gmail.com>
2017-06-10 00:23:16 -0400

    error checks, comments ...

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,24 @@
-var arStack = "";
 
+/* global variables */
+var arStack = "";
+var error = false;
+const errorMsg = "Digit Limit Exceeded"
+
+/* check if number is whole number */
+function isInt(n) {
+   return n % 1 === 0;
+}
+
+/* takes in string representing arithmetic operation and returns results as number */ 
 function getResults(arStack){	
 	var entries = arStack.split(/([\u002B\u00F7\u00D7\u2212])/g);
+    console.log("entries = " + entries);
+    
+    /* for the case where no arithmetic operator is present */
+    if(entries.length === 1){
+        return parseFloat(entries[0]);
+    }
+
     var num1 = entries[0];
     var op = entries[1];
     var num2 = entries[2];
@@ -26,74 +43,57 @@ function getResults(arStack){
       default:
         break;
     }
-  return result;
+
+    /* if result is float we round to 5 decimal places to avoid overflowing display */
+    if(!isInt(result)){
+        return Number(result.toFixed(5));
+    }
+    return result;
 }
 
+/* clears display, errors and any pending operations*/
 function allClear(){
 	arStack = "";
-	$(".display-txt").text("0");
-	$(".running-display").text("0");
+	$(".big-display").text("0");
+	$(".small-display").text("0");
+    error = false;
+}
+
+
+/* check for display overflow */
+function checkLimit() {
+    if ($(".big-display").prop('scrollWidth') > $(".big-display").width() ) {
+            allClear();
+            $(".small-display").text(errorMsg);
+            error = true;
+    }
 }
 
 function smallDisplayOut(str, append){
-	if(append)
+    if(error){
+        return;
+    }
+    if(append)
 	{
-		// var testStr = $(".running-display").text() + str;
-		// if(testStr.length > 28){
-		// 	allClear();
-		// 	smallDisplayOut("Digit Limit Exceeded", false);
-		// 	return;
-		// }
-		$(".running-display").append(str);
-        if ($(".running-display").prop('scrollWidth') > $(".running-display").width() ) {
-            allClear();
-            smallDisplayOut("Digit Limit Exceeded", false);
-            return;
-        }
+		$(".small-display").append(str);
 	}
 	else{
-		// if(str.length > 30){
-		// 	allClear();
-		// 	smallDisplayOut("Digit Limit Exceeded", false);
-		// 	return;
-		// }
-		$(".running-display").text(str);
-        if ($(".running-display").prop('scrollWidth') > $(".running-display").width() ) {
-            allClear();
-            smallDisplayOut("Digit Limit Exceeded", false);
-            return;
-        }
+		$(".small-display").text(str);
 	}
 }
 
 function bigDisplayOut(str, append){
+    if(error){
+        return;
+    }
 	if(append)
 	{
-		//var testStr = $(".display-txt").text() + str;
-		// if(testStr.length > 19){
-		// 	allClear();
-		// 	smallDisplayOut("Digit Limit Exceeded", false);
-		// 	return;
-		// }
-		$(".display-txt").append(str);
-        if ($(".display-txt").prop('scrollWidth') > $(".display-txt").width() ) {
-            allClear();
-            smallDisplayOut("Digit Limit Exceeded", false);
-            return;
-        }
+		$(".big-display").append(str);
+        checkLimit();
 	}
 	else{
-		// if(str.length > 10){
-		// 	allClear();
-		// 	smallDisplayOut("Digit Limit Exceeded", false);
-		// 	return;
-		// }
-		$(".display-txt").text(str);
-        if ($(".display-txt").prop('scrollWidth') > $(".display-txt").width() ) {
-            allClear();
-            smallDisplayOut("Digit Limit Exceeded", false);
-            return;
-        }
+		$(".big-display").text(str);
+        checkLimit();
 	}
 }
 
@@ -103,24 +103,25 @@ $(document).ready(function(){
   
   $(".calc-button").click(function(){
   
+    /* check which button is pressed */
     var input = $(this).children("p:first").text();
     console.log(input);
   
     // if user input is a number
     if(input.match(/[0-9]/g)){ 							
-		if(($(".running-display").text().match(/=/g))){
+		if(($(".small-display").text().match(/=/g))){
 			allClear();
 		}
-		if($(".display-txt").text() === "0"){
+		if($(".big-display").text() === "0"){
             bigDisplayOut(input, false);
-			if($(".running-display").text() === "0"){
+			if($(".small-display").text() === "0"){
 				smallDisplayOut(input, false);
 			}
 			else{
               smallDisplayOut(input, true);
 			}
 		}
-		else if($(".running-display").text().match(/[\u002B\u00F7\u00D7\u2212]$/g)){
+		else if($(".small-display").text().match(/[\u002B\u00F7\u00D7\u2212]$/g)){
 			bigDisplayOut(input, false);
 		  	smallDisplayOut(input, true);
 		}
@@ -133,10 +134,10 @@ $(document).ready(function(){
 
     // if user input is an operator
 	else if(input.match(/[\u002B\u00F7\u00D7\u2212]/g)){ 				
-		if(($(".running-display").text().match(/=/g))){
-			smallDisplayOut($(".display-txt").text(), false);
+		if(($(".small-display").text().match(/=/g))){
+			smallDisplayOut($(".big-display").text(), false);
 		}
-		if(!($(".running-display").text().match(/[\u002B\u00F7\u00D7\u2212]$/g))){
+		if(!($(".small-display").text().match(/[\u002B\u00F7\u00D7\u2212]$/g))){
 			console.log(arStack, arStack.length);
 			if(arStack.split(/([\u002B\u00F7\u00D7\u2212])/g).length === 3){
 				var result = getResults(arStack);
@@ -144,13 +145,19 @@ $(document).ready(function(){
 				arStack = result.toString();
 			}
 			smallDisplayOut(input, true);
-			arStack = arStack.concat(input);
+            if($(".big-display").text() === "0"){
+			    arStack = arStack.concat("0" + input);
+            }
+            else{
+                arStack = arStack.concat(input);
+            }
+            console.log("arStack = ", arStack);
 		}
 	}
 
-    // if user input is .
+    // if user input is "."
 	else if(input === "."){							
-		var test = $(".display-txt").text() + input;
+		var test = $(".big-display").text() + input;
 		if(!test.match(/(\.\.)$/g) && !test.match(/\d+\.\d+\./g)){ 
 			bigDisplayOut(input, true);
 			smallDisplayOut(input, true);
@@ -163,13 +170,13 @@ $(document).ready(function(){
 	}
 
 	else if(input === "CE"){
-		if($(".running-display").text().match(/=/g)){
+		if($(".small-display").text().match(/=/g)){
 			allClear();
 		}
-		else if($(".running-display").text() !== "0"){
-			var regex = new RegExp($(".display-txt").text() + "$", "g"); 
+		else if($(".small-display").text() !== "0"){
+			var regex = new RegExp($(".big-display").text() + "$", "g"); 
 			console.log(regex);
-			smallDisplayOut($(".running-display").text().replace(regex, ''), false);
+			smallDisplayOut($(".small-display").text().replace(regex, ''), false);
 			arStack = arStack.replace(regex, '');
 		}
 		bigDisplayOut("0", false);
@@ -177,7 +184,7 @@ $(document).ready(function(){
 
     // if user input is "="
 	else if(input === String.fromCharCode(61)){ 				
-		if(($(".running-display").text().match(/[0-9]$/g)) && !($(".running-display").text().match(/=/g))){ 
+		if(($(".small-display").text().match(/[0-9]$/g)) && !($(".small-display").text().match(/=/g))){ 
 			var results = getResults(arStack);
 			bigDisplayOut(results, false); 
 			smallDisplayOut("=", true);
